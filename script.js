2401e2456ac87ff2a9fb34c4b2ce67ce4bc47131
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2019-12-12 18:03:19 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+function App() {
+    return (
+        <div className="application-root">
+            <header className="header"/>
+            
+            <main>
+                <section>
+                    <address>
+                        <img src="https://m.media-amazon.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_.jpg" alt="An image of Sally Student" height="200px"/>
+                        <a href="tel:+13115552368">(555)555-5555</a>
+                        <a href="mailto:you@example.com">Email</a>
+                    </address>
+                </section>
+                <section>
+                    <h2>Objective</h2>
+                    <p>I'm an aspiring full stack web developer. I love cats, coding, baking, and lifting things.</p>
+                </section>
+                <section>
+                    <h2>Education</h2>
+                    <p>Boston College, May 2015</p>
+                </section>
+                <section>
+                    <h2>Related Experience</h2>
+                    <h3>Boston College</h3> 
+                    <h4>IT Help Desk Technician, September 2013-May 2015</h4> 
+                    <h3>Family Dental</h3>
+                    <h4>Receptionist, August 2011-August 2013</h4>
+                </section>
+                <section>
+                    <h2>My Skills</h2>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Python</li>
+                    </ul>
+                </section>
+                <section>
+                    <h3>Links</h3>
+                    <p>Github: <a href="https://github.com/Lmcmaaser" > Lmcmaaser</a></p>
+                </section>
+            </main>
+
+        </div>
+    );
+}
+
+const appRoot = document.querySelector('#application-root');
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#application-root'))
